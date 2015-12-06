3c5d95e642f61cccc6c0c0a3a02e28ba9bea43c4
R. Aidan Campbell <aidan.campbell@gmail.com>
2015-12-06 14:23:34 -0500

    ROBOSANTA

--- day3/day3.py
+++ day3/day3.py
@@ -1,18 +1,36 @@
 houses = {}
-x, y = 0, 0
-houses[(x, y)] = 1
+santa_x, santa_y = 0, 0
+robosanta_x, robosanta_y = 0, 0
+houses[(santa_x, santa_y)] = 1
 string = open("input", "r").read()
+ROBO_MODE_ENGAGED = False
 for char_num in range(len(string)):
     character = string[char_num]
     if character is "^":
-        y += 1
+        if ROBO_MODE_ENGAGED:
+            robosanta_y += 1
+        else:
+            santa_y += 1
     elif character is "v":
-        y -= 1
+        if ROBO_MODE_ENGAGED:
+            robosanta_y -= 1
+        else:
+            santa_y -= 1
     elif character is "<":
-        x -= 1
+        if ROBO_MODE_ENGAGED:
+            robosanta_x -= 1
+        else:
+            santa_x -= 1
     elif character is ">":
-        x += 1
-    houses[(x, y)] = houses.setdefault((x, y), 0) + 1
+        if ROBO_MODE_ENGAGED:
+            robosanta_x += 1
+        else:
+            santa_x += 1
+    if ROBO_MODE_ENGAGED:
+        houses[(robosanta_x, robosanta_y)] = houses.setdefault((robosanta_x, robosanta_y), 0) + 1
+    else:
+        houses[(santa_x, santa_y)] = houses.setdefault((santa_x, santa_y), 0) + 1
+    ROBO_MODE_ENGAGED = not ROBO_MODE_ENGAGED
 
 lucky_children = 0
 for house in houses.keys():
