950ed6a259353031560be9db53c1a9b2da6d7382
R. Aidan Campbell <aidan.campbell@gmail.com>
2015-12-06 14:51:17 -0500

    Day 1. Santa can't count floors.

--- day1/day1.py
+++ day1/day1.py
@@ -1,2 +1,11 @@
 string = open("input", "r").read()
+floor = 0
+for char_num in range(len(string)):
+    if string[char_num] is "(":
+        floor += 1
+    else:
+        floor -= 1
+    if floor == -1:
+        print("position where santa went to floor -1: " + str(char_num+1))
+        break
 print(string.count('(') - string.count(")"))
