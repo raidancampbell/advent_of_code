70eb993a700396a50c4b9a69132f558653078701
R. Aidan Campbell <aidan.campbell@gmail.com>
2015-12-06 14:31:59 -0500

    damn non-euclidian elves

--- day2/day2.py
+++ day2/day2.py
@@ -1,9 +1,13 @@
 import re
 total = 0
+ribbon = 0
 for present in open("input", "r").readlines():
     match = re.search("(\d+)x(\d+)x(\d+)", present)
     h = int(match.group(1))
     w = int(match.group(2))
     l = int(match.group(3))
     total += 2 * (l * w + w * h + h * l) + min((l * w, w * h, h * l))
+    ribbon += h * w * l  # freaking non-euclidian elves
+    ribbon += sorted([h, w, l])[0]*2 + sorted([h, w, l])[1]*2
 print(total)
+print(ribbon)
